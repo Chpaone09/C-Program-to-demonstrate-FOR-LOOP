9834b561e5065f727c08e7121a775453dd968e25
Pawankumar Chidurala <chpaone09@hotmail.com>
2020-10-13 02:07:14 +0530

    Program to demonstrate FOR LOOP in c

---
+++ ForLoop.c
@@ -0,0 +1,20 @@
+/*
+***********************************************************
+
+Topic   : Program to demonstrate FOR LOOP in c.
+Author  : Chpaone09®
+Date    : Oct 12, 2020
+
+************************************************************
+*/
+
+#include <stdio.h>
+#include <conio.h>
+
+void main(){
+    int i=0;
+    printf("\n\n\t Demonstration of FOR LOOP in C >> ");
+    for(i=1;i<=5;i++){
+        printf(" %d", i);
+    }
+}
